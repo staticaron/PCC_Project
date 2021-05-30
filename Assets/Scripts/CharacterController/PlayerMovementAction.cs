e20ae5595a8cd057e69efdb07106d8444a539e4b
DevGa.me <devm19102003@gmail.com>
2021-05-30 15:05:29 +0530

    Working : LedgeMovementStop

--- Assets/Scripts/CharacterController/PlayerMovementAction.cs
+++ Assets/Scripts/CharacterController/PlayerMovementAction.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/CelesteController_RB/PlayerMovementAction.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/CharacterController/PlayerMovementAction.inputactions'
 
 using System;
 using System.Collections;
