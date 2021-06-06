6a8c907aee1d0eae5351c685ae1b7977423829de
DevGa.me <devm19102003@gmail.com>
2021-06-06 13:29:47 +0530

    ADD : Guide

---
+++ Assets/DGM/Character Controller/Scripts/Additional/PlayerAnimation.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerController playerController;
+    [SerializeField] private ParticleSystem dashParticleGO;
+    [SerializeField] private List<string> _stateNames;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+
+        PlayerController.EDashed += ToggleDashAnimation;
+        PlayerController.EGrabbed += ToggleGrabAnimation;
+        PlayerController.EJumped += ToggleJumpAnimation;
+        PlayerController.EMovement += ToggleMovementState;
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.EDashed -= ToggleDashAnimation;
+        PlayerController.EGrabbed -= ToggleGrabAnimation;
+        PlayerController.EJumped -= ToggleJumpAnimation;
+        PlayerController.EMovement -= ToggleMovementState;
+    }
+
+    private void ToggleJumpAnimation(bool jumpStarted)
+    {
+        if (jumpStarted) { animator.Play("Launch");  }
+        else { animator.Play("Fall");}
+    }
+
+    private void ToggleDashAnimation(bool start)
+    {
+        if (start == true)
+        {
+            dashParticleGO.Play();
+            animator.Play("Fall");
+        }
+        else
+        {
+            dashParticleGO.Stop();
+        }
+    }
+
+    private void ToggleMovementState(bool isMoving)
+    {
+        if (isMoving)
+        {
+            animator.Play("Run");
+        }
+        else
+        {
+            animator.Play("Idle");
+        }
+    }
+
+    private void ToggleGrabAnimation(bool start)
+    {
+        if (start == true) { animator.Play("Grab"); }
+        else {  }
+    }
+}
